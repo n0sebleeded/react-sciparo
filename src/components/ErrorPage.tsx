2c23a8391cdf699f1297a60e9020be74dd2f46c0
n0sebleeded <devDanilAkhunzianov@outlook.com>
2024-01-09 04:47:14 +0300

    assets upd

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import "./componentsStyles/errorPage.css"
+/*import { Link } from "react-router-dom";*/
+function ErrorPage() {
+    return (
+        <div className="error_page">
+            <p className="error_page_text">PAGE NOT FOUND </p>
+            {/*<Link to="/" className="error_link">Back to the main page</Link>*/}
+        </div>
+    );
+}
+
+export default ErrorPage;
