57bd214b64ed25d165ec2ea3175381fbd2ae1ced
n0sebleeded <devDanilAkhunzianov@outlook.com>
2024-02-03 07:14:50 +0300

    animations upd

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,87 +2,107 @@ import '../../src/components/componentsStyles/login.css'
 import '../../src/components/componentsStyles/btn.css'
 import {useForm} from "react-hook-form";
 import {IFormData} from "../specs/interfaces.tsx";
-import Animated from "./Animated.tsx";
+import { motion } from 'framer-motion';
 // ... (imports and styles)
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors }} = useForm<IFormData>();
+    const popup = {
+        initial: { opacity: 0 },
+        animate: { opacity: 1 },
+        exit: { opacity: 0 },
+    }
 
+    const rotate = {
+        initial: { opacity: 1, rotate: 45 },
+        animate: { opacity: 1, rotate: 0, transition: {duration: 0.5, delay: 1, type: "spring"} },
+    }
     return (
-        <Animated>
-            <div className="login-container">
-                <div className="login-box">
-                    <div className="welcome">
-                        <h1>Welcome!&#128075;</h1>
-                    </div>
-                    <div className="form">
-                        <form onSubmit={handleSubmit((data) => console.log(data))}>
-                            <div className="login-field">
-                                <p>Enter your login: </p>
-                                <input className="inp-format" type="text" id="username" {...register("username", {
-                                    required: "This is a required value",
-                                    minLength: {
-                                        value: 4,
-                                        message: "Minimum length - 4 characters"
-                                    },
-                                    maxLength: {
-                                        value: 15,
-                                        message: "Maximum length - 4 characters"
-                                    },
-                                    pattern: {
-                                        value: /^[a-z0-9_-]{4,15}$/,
-                                        message: "Only english letters or digits"
-                                    }
-                                })}/>
-                            </div>
-                            {
-                                errors.username?.message &&
-                                <p className="error-box">{errors.username?.message}</p>
-                            }
-                            <div className="password-field">
-                                <p>Enter your password: </p>
-                                <input className="inp-format" type="text" id="passw" {...register("password", {
-                                    required: "This field is required",
-                                    minLength: {
-                                        value: 8,
-                                        message: "Minimum length - 8 characters"
-                                    },
-                                    maxLength: {
-                                        value: 20,
-                                        message: "Maximum length - 4 characters"
-                                    },
-                                    pattern: {
-                                        value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/,
-                                        message: "Password must contain a letter and a digit",
-                                    }
-                                })} />
-                            </div>
-                            {
-                                errors.password?.message &&
-                                <p className="error-box">{errors.password?.message}</p>
-                            }
-                            <div className="email-field">
-                                <p>Enter your email: </p>
-                                <input className="inp-format-rev" {...register("email", {
-                                    required: "This field is required",
-                                    pattern: {
-                                        value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-                                        message: "Incorrect value"
-                                    }
-                                })} type="text" id="emai"/>
-                            </div>
-                            {
-                                errors.email?.message &&
-                                <p className="error-box">{errors.email?.message}</p>
-                            }
-                            <div className="button-field">
-                                <button type="submit" className="btn btn-white btn-animate">submit</button>
-                            </div>
-                        </form>
-                    </div>
+        <motion.div className="login-container" variants={popup}
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    transition={{duration: 1}}
+        >
+            <div className="login-box">
+                <div className="welcome">
+                    <h1 className="phrase-emoji">
+                        Welcome!
+                        <motion.div className="emoji-hand" variants={rotate}
+                                     initial="initial"
+                                     animate="animate"
+                        >
+                            <span>&#128075;</span>
+                        </motion.div>
+                    </h1>
+                </div>
+                <div className="form">
+                    <form onSubmit={handleSubmit((data) => console.log(data))}>
+                        <div className="login-field">
+                            <p>Enter your login: </p>
+                            <input className="inp-format" type="text" id="username" {...register("username", {
+                                required: "This is a required value",
+                                minLength: {
+                                    value: 4,
+                                    message: "Minimum length - 4 characters"
+                                },
+                                maxLength: {
+                                    value: 15,
+                                    message: "Maximum length - 4 characters"
+                                },
+                                pattern: {
+                                    value: /^[a-z0-9_-]{4,15}$/,
+                                    message: "Only english letters or digits"
+                                }
+                            })}/>
+                        </div>
+                        {
+                            errors.username?.message &&
+                            <p className="error-box">{errors.username?.message}</p>
+                        }
+                        <div className="password-field">
+                            <p>Enter your password: </p>
+                            <input className="inp-format" type="text" id="passw" {...register("password", {
+                                required: "This field is required",
+                                minLength: {
+                                    value: 8,
+                                    message: "Minimum length - 8 characters"
+                                },
+                                maxLength: {
+                                    value: 20,
+                                    message: "Maximum length - 4 characters"
+                                },
+                                pattern: {
+                                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/,
+                                    message: "Password must contain a letter and a digit",
+                                }
+                            })} />
+                        </div>
+                        {
+                            errors.password?.message &&
+                            <p className="error-box">{errors.password?.message}</p>
+                        }
+                        <div className="email-field">
+                            <p>Enter your email: </p>
+                            <input className="inp-format-rev" {...register("email", {
+                                required: "This field is required",
+                                pattern: {
+                                    value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                                    message: "Incorrect value"
+                                }
+                            })} type="text" id="emai"/>
+                        </div>
+                        {
+                            errors.email?.message &&
+                            <p className="error-box">{errors.email?.message}</p>
+                        }
+                        <div className="button-field">
+                            <button type="submit" className="btn btn-white btn-animate">submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </Animated>
+        </motion.div>
     );
 };
 
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,35 +1,101 @@
 import './componentsStyles/mainPage.css';
-import Animated from "./Animated.tsx";
+import { motion } from 'framer-motion';
 
 const MainPage = () => {
+    const vertical = {
+        initial: { opacity: 0, y: 100 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: -100 },
+    };
+
+    const horizontal = {
+        initial: { opacity: 0, x: 100 },
+        animate: { opacity: 1, x: 0 },
+        exit: { opacity: 0, x: -100 },
+    };
+
+    const fade = {
+        initial: { opacity: 0 },
+        animate: { opacity: 1 },
+        exit: { opacity: 0 },
+    }
     //TODO: will be redesign
     return (
-        <Animated>
-            <div className="main-container">
-                <div className="sub-container">
-                    <div className="leftside-container">
-                        <h1 className="main-text">
+        <div className="main-container">
+            <div className="sub-container">
+                <div className="leftside-container">
+                    <h1 className="main-text">
+                        <motion.div
+                            variants={vertical}
+                            initial="initial"
+                            animate="animate"
+                            exit="exit"
+                            transition={{duration: 1}}
+                        >
                             <span className="rock">ROCK-</span><br/>
+                        </motion.div>
+                        <motion.div
+                            variants={vertical}
+                            initial="initial"
+                            animate="animate"
+                            exit="exit"
+                            transition={{duration: 1, delay: 0.5}}
+                        >
                             <span className="paper">PAPER-</span><br/>
-                            <span className="scissor">SCISSOR</span>
-                        </h1>
-                    </div>
-                    <div className="rightside-container">
-                        <div className="main-img-container">
+                        </motion.div>
+                        <motion.div
+                            variants={vertical}
+                            initial="initial"
+                            animate="animate"
+                            exit="exit"
+                            transition={{duration: 1, delay:1}}
+                        >
+                            <span className="scissor">SCISSOR</span><br/>
+                        </motion.div>
+                    </h1>
+                </div>
+                <div className="rightside-container">
+                    <div className="main-img-container">
+                        <motion.div variants={horizontal}
+                                    initial="initial"
+                                    animate="animate"
+                                    exit="exit"
+                                    transition={{duration: 1}}
+                        >
                             <img className="main-img" src="../../src/assets/card_rock.svg" alt="rock"/>
+                        </motion.div>
+                        <motion.div variants={horizontal}
+                                    initial="initial"
+                                    animate="animate"
+                                    exit="exit"
+                                    transition={{duration: 1, delay: 0.5}}
+                        >
                             <img className="main-img" src="../../src/assets/card_paper.svg" alt="paper"/>
+                        </motion.div>
+                        <motion.div variants={horizontal}
+                                    initial="initial"
+                                    animate="animate"
+                                    exit="exit"
+                                    transition={{duration: 1, delay: 1}}
+                        >
                             <img className="main-img" src="../../src/assets/card_scissor.svg" alt="scissor"/>
-                        </div>
+                        </motion.div>
+                    </div>
+                    <motion.div variants={fade}
+                                initial="initial"
+                                animate="animate"
+                                exit="exit"
+                                transition={{duration: 2, delay: 0}}>
                         <p className="sub-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ligula
                             libero,
                             semper sed turpis in, <span className="highlighted-text">aliquam consequat odio</span>. Ut
                             ullamcorper mauris ex, ac tincidunt elit
                             euismod a👏👏
                         </p>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
-        </Animated>
+        </div>
     );
 };
 
